839c97417553f8896fba127b85ab6216158c129f
Benned-H <benned1@gmail.com>
2024-05-16 16:34:02 -0400

    Implement Wilson's algorithm for uniform spanning trees

---
+++ src/graphs/graph_partition.py
@@ -0,0 +1,54 @@
+"""This module provides methods to partition a given undirected graph."""
+
+import numpy as np
+from graphs.undirected_graph import T, UndirectedGraph
+
+
+def uniform_spanning_tree(graph: UndirectedGraph[T]) -> UndirectedGraph:
+    """Compute a uniform spanning tree using Wilson's algorithm.
+
+    A spanning tree T of graph G is a tree subgraph that includes all vertices in G.
+
+    A uniform spanning tree of graph G is a spanning tree chosen uniformly
+        at random from all possible spanning trees of G.
+
+    :param      graph   Undirected graph for which a uniform spanning tree is found
+    :returns    Undirected graph representing a random spanning tree of G
+    """
+
+    # Begin with an empty tree (as if none of the vertices are in the tree)
+    tree = UndirectedGraph[T](graph.V, [])
+    in_tree = np.full((graph.size_V), False, dtype=bool)  # Boolean array of shape (V,)
+
+    # Sample a random root for the tree and mark it as in the tree
+    rng = np.random.default_rng()
+    root = rng.integers(graph.size_V)
+    in_tree[root] = True
+
+    # Track the next vertex during random walks back to the tree
+    next_v = np.full((graph.size_V), -1, dtype=int)  # -1 means "not initialized"
+
+    # Generate a random permutation of the vertex indices in G
+    for i in rng.permutation(graph.size_V):
+        u = i  # First, random walk from i until we reach a vertex in the tree
+
+        while not in_tree[u]:
+            next_v[u] = graph.random_neighbor(u, rng)  # Overwrite cycles if they occur
+            u = next_v[u]
+
+        u = i  # Then, retrace the walk and add it to the tree
+        while not in_tree[u]:
+            new_edge = (u, next_v[u])
+            tree.add_edge(new_edge)  # Add the edge to the output tree object
+
+            in_tree[u] = True
+            u = next_v[u]
+
+        # Exit early if all vertices are already in the tree
+        if np.all(in_tree):
+            break
+
+    # Verify expected properties before exiting
+    assert np.all(in_tree), "All nodes should be marked as in the spanning tree!"
+
+    return tree
--- src/graphs/undirected_graph.py
+++ src/graphs/undirected_graph.py
@@ -1,5 +1,6 @@
 """This module implements a generic undirected graph using adjacency lists."""
 
+import numpy as np
 from typing import TypeVar, Generic
 
 T = TypeVar("T")
@@ -8,7 +9,7 @@ T = TypeVar("T")
 class UndirectedGraph(Generic[T]):
     """A generic undirected graph composed of vertices and bidirectional edges."""
 
-    def __init__(self, vertices=list[T], edges=list[tuple[int, int]]):
+    def __init__(self, vertices: list[T], edges: list[tuple[int, int]]):
         """Initialize the undirected graph, given vertex and edge lists.
 
         All edges (i,j) will be added as both (i,j) and (j,i) in the graph.
@@ -28,10 +29,44 @@ class UndirectedGraph(Generic[T]):
             self.adjacent[i].add(j)
             self.adjacent[j].add(i)
 
-    def vertex_count(self):
-        """Return the number of vertices in the graph."""
-        return len(self.V)
+        # Compute and store the number of vertices and edges in the graph
+        self.size_V: int = len(self.V)
+        self.size_E: int = sum([len(adj) for adj in self.adjacent])
 
-    def edge_count(self):
-        """Return the number of edges in the graph."""
-        return sum([len(adj) for adj in self.adjacent])
+    def add_vertex(self, data: T):
+        """Create a new vertex containing the given data.
+
+        :param      data        Generic data stored in the new vertex
+        """
+        self.V.append(data)
+        self.adjacent.append(set())  # New vertex begins with no adjacent vertices
+
+        self.size_V += 1
+
+    def add_edge(self, edge: tuple[int, int]):
+        """Add the given edge (i,j) to the undirected graph.
+
+        :param      edge        Edge (i,j) connecting two vertices (i and j)
+        """
+        (i, j) = edge
+
+        assert 0 <= i and i < self.size_V, f"Given vertex index {i} not in graph!"
+        assert 0 <= j and j < self.size_V, f"Given vertex index {j} not in graph!"
+
+        self.adjacent[i].add(j)
+        self.adjacent[j].add(i)
+
+        self.size_E += 2
+
+    def random_neighbor(self, u: int, rng: np.random.Generator) -> int:
+        """Sample a random neighbor of the given vertex.
+
+        :param      u           Index of the vertex to sample a neighbor for
+        :param      rng         Random number generator (initialized elsewhere)
+        :returns    neighbor    Index of a random neighbor of vertex u
+        """
+        neighbors = list(self.adjacent[u])
+        random_idx = rng.integers(len(neighbors))  # Index into u's neighbors list
+        neighbor = neighbors[random_idx]
+
+        return neighbor
