1894c350dc43f16b310893cb655789dcc49edf01
Benned Hedegaard <benned1@gmail.com>
2024-05-21 13:40:11 -0400

    Implement concrete A* planner for Four Rooms environment

--- src/optimal_behaviors/abstract_a_star.py
+++ src/optimal_behaviors/abstract_a_star.py
@@ -4,23 +4,26 @@ from typing import TypeVar, Generic
 from abc import ABC, abstractmethod
 
 StateT = TypeVar("StateT")
+NodeT = TypeVar("NodeT")
 
 
 class Node(Generic[StateT], ABC):
     """An abstract, generic node data structure used during A* search."""
 
-    def __init__(self, state: StateT):
-        """Initialize the node using its stored state.
+    def __init__(self, state: StateT, prev: Node[StateT], a_cost: float, h: float):
+        """Initialize the node using its stored state and A*-relevant data.
 
-        :param      state       State within the space being searched
+        :param      state       State to be stored in this node
+        :param      prev        Previous node during A* search
+        :param      a_cost      Cost of the action from the previous node to this node
+        :param      h           Heuristic estimate of cost-to-go from this node
         """
         self.state = state
 
-        # We don't initialize these upon construction in case the node is closed
-        self.g = 0  # Cost-to-reach g
-        self.f = 0  # Estimated cost of entire path through the node, f = g + h(s)
+        self.prev = prev  # Pointer to parent node during A* search
 
-        self.prev = None  # Pointer to parent node during A* search
+        self.g = prev.g + a_cost  # Cost to reach this node
+        self.f = self.g + h  # Estimated total cost through this node
 
     def __str__(self) -> str:
         """Create a human-readable string representing this node."""
@@ -39,13 +42,14 @@ class Node(Generic[StateT], ABC):
         pass
 
 
-class AStarPlanner(Generic[StateT], ABC):
+class AStarPlanner(Generic[StateT, NodeT], ABC):
     """An abstract A* planner over a generic state representation."""
 
     def __init__(self):
         """Initialize the A* planner's necessary member variables."""
         self.open_list: list[Node[StateT]] = []
         self.closed_list: list[Node[StateT]] = []
+        self.goals: set[StateT] = set()
 
     def backtrack(self, node: Node[StateT]) -> list[Node[StateT]]:
         """Backtrack from the given node to find the path it represents.
@@ -83,79 +87,89 @@ class AStarPlanner(Generic[StateT], ABC):
 
                 return  # The open list should only store at most one node per state
 
+    def state_closed(self, state: StateT):
+        """Check whether any node in the closed list contains the given state.
+
+        :param      state       State that may or may not already be closed
+        :returns    Boolean indicating if the state has been closed during A* search
+        """
+        return any([node for node in self.closed_list if node.state == state])
+
     @abstractmethod
-    def neighbors(self, node: Node[StateT]) -> set[Node[StateT]]:
-        """Find the neighboring nodes of the given node.
+    def unclosed_neighbors(self, node: Node[StateT]) -> set[Node[StateT]]:
+        """Find the unclosed neighboring nodes of the given node.
 
         This method simplifies the overall A* process by combining what could have been
             two functions, actions(n) and result(n, a), into one method.
-
-        TODO: Could skip closed neighbors at the source! I get it now! Allows us to
-            build the action, cost, and f/g/h into each Node from the beginning.
 
         TODO: This method is placed into the A* planner, not the Node class, because
             we don't want to pass the environment's dynamics to every Node. It would
             be better to have an abstract StateSpace class, but this is fine for now.
 
-        Note: Only sets neighbor.state, but doesn't touch g, f, or prev.
-
         :param      node        Node during A* search whose neighbors are expanded
-        :returns    Set of nodes resulting from valid actions from the given node
+        :returns    Set of unclosed nodes resulting from valid actions from the node
         """
         pass
 
     @abstractmethod
-    def cost(self, n1: Node[StateT], n2: Node[StateT]) -> float:
-        """Find the cost to move from the current node (n1) to the next node (n2).
+    def cost(self, s1: StateT, s2: StateT) -> float:
+        """Find the cost to move from one state (s1) to another state (s2).
 
-        :param      n1          Current node an action was taken from
-        :param      n2          Next node reached by that action
-        :returns    Cost of the action between the two nodes
+        :param      s1          Current state an action was taken from
+        :param      s2          Next state reached by that action
+        :returns    Cost of the action between the two states
         """
         pass
 
     @abstractmethod
-    def h(self, node: Node[StateT], G: set[StateT]) -> float:
-        """Compute a heuristic estimate of the cost-to-go from the given node.
+    def h(self, state: StateT, goals: set[StateT]) -> float:
+        """Compute a heuristic estimate of the cost-to-go from the given state.
 
-        If there are multiple states in G, the minimum-distance goal is used.
+        If there are multiple goal states, the minimum-distance goal is used.
 
-        :param      node        Node from which cost-to-go is estimated
-        :param      G           Set of goal states
+        TODO: Separate class for heuristic function, naturally.
+
+        :param      state       State from which cost-to-go is estimated
+        :param      goals       Set of goal states
         :returns    Estimated cost-to-go (must be optimistic)
         """
         pass
 
-    def a_star(self, s0: StateT, G: set[StateT]) -> list[Node[StateT]]:
+    @abstractmethod
+    def reset(self, s0: StateT, goals: set[StateT]):
+        """Set up the A* planner for search on the given planning problem.
+
+        The open list begins as [s0] and the closed list begins empty.
+
+        :param      s0      Initial state in the planning problem
+        :param      goals   Set of goal states to be reached
+        """
+        pass
+
+    def a_star(self, s0: StateT, goals: set[StateT]) -> list[Node[StateT]]:
         """Run A* search on the given state space search problem.
 
         This function works with the abstract functions outlined above.
 
         :param      s0      Initial state for the search problem
-        :param      G       Set of goal states to be reached
-        :returns    Optimal path (list of nodes) from s0 to some goal state in G
+        :param      goals   Set of goal states to be reached
+        :returns    Optimal path (list of nodes) from s0 to some goal state
         """
-        self.open_list = [Node[StateT](s0)]  # Create a node for the starting state
-        self.closed_list.clear()
+        self.reset(s0, goals)  # Create a node for the starting state and store goals
 
         while self.open_list:  # Continue until the open list is empty
             self.open_list.sort(key=lambda node: node.f)
 
             curr = self.open_list.pop(0)
 
-            if curr.state in G:  # Does this node contain a goal state?
+            if curr.state in goals:  # Does this node contain a goal state?
                 return self.backtrack(curr)
 
             self.closed_list.append(curr)
 
-            for n in self.neighbors(curr):
-
-                # If the neighbor isn't already closed, add it to the open list
-                if n not in self.closed_list:
-                    n.g = curr.g + self.cost(curr, n)  # g is cost-to-reach
-                    n.f = n.g + self.h(curr, G)  # f is total-cost-through
-                    n.prev = curr  # Neighbor was preceded by the current node
-                    self.push_open_list(n)
+            # For each neighbor that isn't already closed, add it to the open list
+            for n in self.unclosed_neighbors(curr):
+                self.push_open_list(n)
 
         # If we exit the while loop and haven't returned, search has failed
         return []  # Empty path indicates failure
---
+++ src/optimal_behaviors/four_rooms_a_star.py
@@ -0,0 +1,125 @@
+"""This module defines a concrete A* planner for the Four Rooms environment."""
+
+from typing import NewType
+import numpy as np
+
+from envs.four_rooms import FourRoomsEnv
+from graphs.state_transition_graph import get_transition_graph
+from optimal_behaviors.abstract_a_star import Node, AStarPlanner
+
+# We'll perform A* search over vertex indices in the transition graph
+StateV = NewType("StateV", int)
+
+
+class FourRoomsNode(Node[StateV]):
+    """A concrete node to structure A* search in the Four Rooms environment."""
+
+    def __init__(self, state: StateV, prev: Node[StateV], a_cost: float, h: float):
+        """Initialize the node using its stored state and A*-relevant data.
+
+        :param      state       State to be stored in this node
+        :param      prev        Previous node during A* search
+        :param      a_cost      Cost of the action from the previous node to this node
+        :param      h           Heuristic estimate of cost-to-go from this node
+        """
+        super().__init__(state, prev, a_cost, h)
+
+    def __eq__(self, other: Node[StateV]) -> bool:
+        """Check whether the given node contains the same state.
+
+        Overrides the `==` operator for these objects, allowing "node equality" to
+            ignore the internal cost-to-reach g and estimated total cost f.
+
+        :param      other       Another node to compare equality against
+        :returns    Boolean indicating if the nodes' stored states are equal
+        """
+        return self.state == other.state
+
+
+class FourRoomsAStarPlanner(AStarPlanner[StateV]):
+    """A concrete A* planner for the Four Rooms environment."""
+
+    def __init__(self, env: FourRoomsEnv):
+        """Initialize the A* planner using the abstract class constructor.
+
+        :param      env     Four Rooms environment defining state transition graph
+        """
+        self.transition_graph = get_transition_graph(env)
+
+        super().__init__()
+
+    def unclosed_neighbors(self, node: Node[StateV]) -> set[Node[StateV]]:
+        """Find the unclosed neighboring nodes of the given node.
+
+        For the Four Rooms environment, neighboring states during A* will correspond
+            to neighboring vertex indices in the state transition graph.
+
+        TODO: This method is placed into the A* planner, not the Node class, because
+            we don't want to pass the environment's dynamics to every Node. It would
+            be better to have an abstract StateSpace class, but this is fine for now.
+
+        :param      node        Node during A* search whose neighbors are expanded
+        :returns    Set of unclosed nodes resulting from valid actions from the node
+        """
+        neighbors = self.transition_graph.adjacent[node.state]
+        unclosed_neighbors = [n_v for n_v in neighbors if not self.state_closed(n_v)]
+
+        # Now convert the neighboring states (v_idxs) into Node objects
+        neighbor_nodes = set()
+        for neighbor_state in unclosed_neighbors:
+            action_cost = self.cost(node.state, neighbor_state)
+            h_value = self.h(neighbor_state, self.goals)
+            neighbor_node = FourRoomsNode(neighbor_state, node, action_cost, h_value)
+            neighbor_nodes.add(neighbor_node)
+
+        return neighbor_nodes
+
+    def cost(self, s1: StateV, s2: StateV) -> float:
+        """Find the cost to move from one state (s1) to another state (s2).
+
+        In the Four Rooms environment, assume action cost is Euclidean distance.
+
+        :param      s1          Current state an action was taken from
+        :param      s2          Next state reached by that action
+        :returns    Cost of the action between the two states
+        """
+        s1_xy = self.transition_graph.V[s1]  # These states represent vertex indices
+        s2_xy = self.transition_graph.V[s2]
+
+        distance_m = np.linalg.norm(s1_xy - s2_xy)
+
+        return distance_m
+
+    def h(self, state: StateV, goals: set[StateV]) -> float:
+        """Compute a heuristic estimate of the cost-to-go from the given state.
+
+        If there are multiple goal states, the minimum-distance goal is used. In the
+            Four Rooms environment, Euclidean distance is used as the heuristic.
+
+        TODO: Separate class for heuristic function, naturally.
+
+        :param      state       State from which cost-to-go is estimated
+        :param      goals       Set of goal states
+        :returns    Estimated cost-to-go (must be optimistic)
+        """
+        assert len(goals) >= 1, "Estimating h(s) requires at least one goal!"
+
+        state_xy = self.transition_graph.V[state]  # Convert v_idx into (x,y)
+        goals_xy = [self.transition_graph.V[v_idx] for v_idx in goals]
+
+        distances_m = [np.linalg.norm(state_xy - g_xy) for g_xy in goals_xy]
+        min_distance_m = min(distances_m)
+
+        return min_distance_m
+
+    def reset(self, s0: StateV, goals: set[StateV]):
+        """Set up the A* planner for search on the given planning problem.
+
+        The open list begins as [s0] and the closed list begins empty.
+
+        :param      s0      Initial state in the planning problem
+        :param      goals   Set of goal states to be reached
+        """
+        self.open_list = [FourRoomsNode(s0, None, 0.0, self.h(s0, goals))]
+        self.closed_list.clear()
+        self.goals = goals
